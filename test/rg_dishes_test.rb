237dabb1a8b0c610b8a276aee554b4e160488d62
Rachel <rebgrewcock@gmail.com>
2021-03-07 18:33:11 +0000

    Finish version 1.

--- test/rg_dishes_test.rb
+++ test/rg_dishes_test.rb
@@ -4,32 +4,32 @@ require "test_helper"
 
 class RgDishesTest < Minitest::Test
   def test_inspire_returns_something
-    output = RgDishes.inspire
+    output = RgDishes::inspire
     refute output.empty?
   end
 
   def test_inspire_returns_string
-    output = RgDishes.inspire
+    output = RgDishes::inspire
     assert_equal String, output.class
   end
 
   def test_combine_with_known_dish
-    output = RgDishes.combine(["carbonara", "menemen"])
+    output = RgDishes::combine(["carbonara", "menemen"])
     assert_equal "bacon\nbread\negg (*2 dishes)\npecorino\npepper\nspaghetti\ntomato", output
   end
 
   def test_combine_with_unknown_dish
-    output = RgDishes.combine(["bob"])
+    output = RgDishes::combine(["bob"])
     assert_equal "I don't know the ingredients for bob.", output
   end
 
   def test_combine_known_and_unknown
-    output = RgDishes.combine(["carbonara", "bob"])
+    output = RgDishes::combine(["carbonara", "bob"])
     assert_equal "bacon\negg\npecorino\nspaghetti\nI don't know the ingredients for bob.", output
   end
 
   def test_combine_no_dishes
-    output = RgDishes.combine("")
+    output = RgDishes::combine("")
     assert_equal "Please include a dishy argument.", output
   end
 end
