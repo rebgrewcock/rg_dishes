8b23d36c8f973c4e9d2dd8493274261aa208f020
Rachel <rebgrewcock@gmail.com>
2021-03-07 13:09:40 +0000

    Make tests fail.

--- lib/rg_dishes.rb
+++ lib/rg_dishes.rb
@@ -2,7 +2,146 @@
 
 require_relative "rg_dishes/version"
 
+require 'csv'
+
 module RgDishes
   class Error < StandardError; end
-  # Your code goes here...
+
+  #takes in dish name and returns its ingredients string.
+  def dish_to_ingredients(dish_key, data)
+    if data[dish_key.downcase]
+      ingredients = data[dish_key.downcase].join(", ")
+    else
+      ingredients = nil
+    end
+    ingredients
+  end
+
+  #returns ingredients sentence from dish name and ingredients
+  def construct_ingredients_sentence(ingredients, dish_arg)
+    if ingredients
+      ingredients_sentence = "Ingredients for #{dish_arg}: #{ingredients}."
+    else
+      ingredients_sentence = "I don't know the ingredients for #{dish_arg}."
+    end
+    ingredients_sentence
+  end
+
+  #takes in dish_arg, dish_key, returns ingredients sentence
+  def dish_to_sentence(dish_arg, dish_key, data)
+    ingredients = dish_to_ingredients(dish_key, data)
+    sentence = construct_ingredients_sentence(ingredients, dish_arg)
+    sentence
+  end
+
+  #returns the names of all dishes in hash
+  def list_all_dishes(table)
+    dishes = table.keys
+    dishes
+  end
+
+  #returns hash{ingredient => number}
+  def build_count_hash(ingredients)
+    ing_num_hash = {}
+    ingredients.each do |ingredient|
+      if ing_num_hash[ingredient]
+        ing_num_hash[ingredient] += 1
+      else
+        ing_num_hash[ingredient] = 1
+      end
+    end
+    ing_num_hash
+  end
+
+  #returns array of strings for ingredients with number (where >2 dishes)
+  def build_counted_dishes_array(ingredients_num_hash)
+    ing_array = []
+    ingredients_num_hash.each do |ingr, num|
+      if num > 1
+        ingr_string = ingr + " (*" + num.to_s + " dishes)"
+      else
+        ingr_string = ingr
+      end
+      ing_array << ingr_string
+    end
+    ing_array
+  end
+
+  #returns shopping list array
+  def make_shopping_list(ingr_array, no_ingr_dishes)
+    ingredients_string = ingr_array.uniq.sort.join("\n")
+    shopping_list = []
+    shopping_list << ingredients_string
+    if !no_ingr_dishes.empty?
+      shopping_list << "I don't know the ingredients for #{no_ingr_dishes.join(", ")}."
+    end
+    shopping_list
+  end
+
+  #----------------
+
+  #reads csv into array of arrays.
+  #doc = CSV.read("dishes.csv")
+
+  doc = [["carbonara", "spaghetti", "bacon", "egg", "pecorino", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], ["menemen", "egg", "pepper", "tomato", "bread", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], ["sausages in a tray", "sausages", "sweet potato", "onion", "apple", "herbs", nil, nil, nil, nil, nil, nil, nil, nil, nil], ["stir fry", "noodles", "stir fry vegetables", "tofu", "fish sauce", "soy sauce", "garlic", "ginger", nil, nil, nil, nil, nil, nil, nil], ["spaghetti alle cozze", "spaghetti", "mussels", "white wine", "garlic", "chili flakes", "parsley", nil, nil, nil, nil, nil, nil, nil, nil], ["risotto", "risotto rice", "white wine", "stock", "herbs", "butter", nil, nil, nil, nil, nil, nil, nil, nil, nil], ["salmon en croute", "salmon", "pastry", "soft cheese", "lemon juice", "dill", "watercress", "egg", nil, nil, nil, nil, nil, nil, nil], ["grilled aubergine", "aubergine", "lemon", "garlic", "oregano", "basil", nil, nil, nil, nil, nil, nil, nil, nil, nil], ["aubergine parmigiana", "aubergine", "shallot", "garlic", "canned tomatoes", "basil", "parmesan", "chili flakes", nil, nil, nil, nil, nil, nil, nil], ["caramelized onion and blue cheese cups", "pastry", "red onion", "cider vinegar", "brown sugar", "blue cheese", "walnuts", "egg", nil, nil, nil, nil, nil, nil, nil], ["tuna steak", "tuna steak", "garlic", "ginger", "soy sauce", "orange juice", "lemon juice", nil, nil, nil, nil, nil, nil, nil, nil], ["roast potatoes", "potatoes", "herbs", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], ["roast carrots and parsnips", "carrots", "parsnips", "honey", "thyme", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], ["Hasselbach potatoes", "potatoes", "lemon", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], ["roast chicken with a mustard glaze", "chicken", "garlic", "rosemary", "mustard", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], ["roast chicken with apple and brandy", "chicken", "garlic", "brandy", "apple juice", "onion", nil, nil, nil, nil, nil, nil, nil, nil, nil], ["baked sardines", "sardines", "garlic", "herbs", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], ["Beef Wellington", "fillet steak", "shallot", "chestnut mushrooms", "porcini mushrooms", "butter", "thyme", "white wine", "prosciutto", "pastry", "egg", "bay leaf", "stock", "brandy", "red wine"], ["glazed salmon", "salmon", "brown sugar", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], ["pesto", "pine nuts", "garlic", "basil", "parmesan", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil], ["Ratatouille", "aubergine", "pepper", "courgette", "tomato", "onion", "garlic", "basil", "thyme", "lemon", "cumin", nil, nil, nil, nil]]
+
+  #transforms array of arrays into a hash.
+  table = {}
+  doc.each do |array|
+    table[array.shift] = array.compact
+  end
+
+  #transforms all keys in hash to lower case.
+  table.transform_keys!(&:downcase)
+
+  #----------------
+
+  #dinner inspire returns all dishes in hash
+  def inspire
+    return list_all_dishes(table).join("\n")
+  end
+
+  #----------------
+
+  #dinner combine returns ingredients for all dishes
+  def combine(dish_array)
+    if !dish_array.empty?
+      ## TODO: array includes empty string.
+      ingredients = []
+      no_ingredients_dishes = []
+      dish_array.each do |dish_element|
+        arg_string = dish_element.downcase
+        if table[arg_string]
+          #concat makes array rather than array of arrays
+          ingredients.concat(table[arg_string])
+        else
+          no_ingredients_dishes << dish_element
+        end
+      end
+    else
+      abort("Please include a dishy argument.")
+    end
+
+    #builds hash to count ingredients
+    ingredients_num_hash = build_count_hash(ingredients)
+
+    #appends number to repeated ingredients
+    ingredients_array = build_counted_dishes_array(ingredients_num_hash)
+
+    #builds shopping list
+    shopping_list = make_shopping_list(ingredients_array, no_ingredients_dishes).join("\n")
+
+    #returns shopping list_all_dishes
+    return shopping_list
+  end
+
+  #----------------
+end
+
+class String
+  include RgDishes
+end
+
+class Integer
+  include RgDishes
 end
--- test/rg_dishes_test.rb
+++ test/rg_dishes_test.rb
@@ -3,11 +3,43 @@
 require "test_helper"
 
 class RgDishesTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::RgDishes::VERSION
+  def test_inspire_returns_something
+    output = inspire
+    refute output.empty?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_inspire_returns_string
+    output = inspire
+    assert_equal "String", output.class
+  end
+
+  def test_combine_with_known_dish
+    output = combine(["carbonara", "menemen"])
+    assert_equal "bacon
+                  bread
+                  egg (*2 dishes)
+                  pecorino
+                  pepper
+                  spaghetti
+                  tomato", output
+  end
+
+  def test_combine_with_unknown_dish
+    output = combine(["bob"])
+    assert_equal "I don't know the ingredients for bob.", output
+  end
+
+  def test_combine_known_and_unknown
+    output = combine(["carbonara", "bob"])
+    assert_equal "bacon
+                  egg
+                  pecorino
+                  spaghetti
+                  I don't know the ingredients for bob.", output
+  end
+
+  def test_combine_no_dishes
+    output = combine("")
+    assert_equal "Please include a dishy argument.", output
   end
 end
